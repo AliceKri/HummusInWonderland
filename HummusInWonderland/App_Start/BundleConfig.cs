499eab12d581bef4b77be0a67a7e7f0da2914ffb
dh94 <halleldaniel@gmail.com>
2016-11-12 17:16:21 +0200

    HomePage

--- HummusInWonderland/App_Start/BundleConfig.cs
+++ HummusInWonderland/App_Start/BundleConfig.cs
@@ -21,11 +21,14 @@ namespace HummusInWonderland
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/slick.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/slick.css",
+                      "~/Content/slick-theme.css"));
         }
     }
 }
